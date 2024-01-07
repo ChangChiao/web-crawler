6edcfc2af299a3f7cc92d3a0b2fc9d845ff0ba7f
JoeCT_Chang <JoeCT.Chang@moxa.com>
2024-01-07 21:19:08 +0800

    feat: add selenium

--- crawler.py
+++ crawler.py
@@ -1,10 +1,15 @@
 import os
 import smtplib
 import requests
+import time
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
 
 load_dotenv()
 
@@ -13,7 +18,8 @@ load_dotenv()
 url =  os.getenv('GOODS_URL')
 
 # product name
-target_product = "FC660M"
+# target_product = "FC660M"
+target_product = "FC980M"
 
 # email
 email_sender = os.getenv('EMAIL_USER')
@@ -41,18 +47,40 @@ def send_email(subject, body):
         server.sendmail(email_sender, email_receiver, msg.as_string())
 
         server.quit()
-def track_product():
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    body_content = soup.body
-    print(f'soup: {body_content}')
 
-    product_elements = soup.body.find_all('div', string=target_product)  
-    if product_elements:
+# def track_product():
+#     response = requests.get(url)
+#     soup = BeautifulSoup(response.text, 'html.parser')
+#     body_content = soup.body
+#     print(f'soup: {body_content}')
+
+#     product_elements = soup.body.find_all('div', string=target_product)  
+#     if product_elements:
+#         print(f'fetch success')
+#         # send_email("商品通知", f"{target_product}已經推出！")
+#     else:
+#         print('Product not found')
+
+def track_product():
+    driver = webdriver.Chrome()
+    print("=" * 100)
+    driver.get(url)
+    time.sleep(10)
+
+    try:
+        element = driver.find_element(By.XPATH,"//a[contains(text(),'"+target_product+"')]")
         print(f'fetch success')
-        send_email("商品通知", f"{target_product}已經推出！")
-    else:
+        # send_email("商品通知", f"{target_product}已經推出！")
+
+    except:
         print('Product not found')
+
+    finally:
+        driver.quit()
+
+    # button = driver.find_element(By.XPATH, "//span[contains(text(),'韓國 Leopold']")
+    # print(f'button==> {button}')
+
 
 
 if __name__ == "__main__":
